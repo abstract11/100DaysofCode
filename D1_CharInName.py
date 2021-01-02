2b716630b1668c2ef601d1db39beeea93a3d97c1
Aditya Pal <paladitya304@gmail.com>
2021-01-02 11:54:01 +0530

    Add files via upload

---
+++ D1_CharInName.py
@@ -0,0 +1,11 @@
+#approch 1
+#name_length = input("What is your name? ")
+#l = len(name_length) #Gets the number of characters in a word with spaces.
+#print(l)
+
+#approach 2
+#l = len(input("What is your name? ")) #Gets the number of characters in a word with spaces.
+#print(l)
+
+#approach 3
+print(len(input("What is your name? ")))
